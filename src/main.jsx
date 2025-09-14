4d61161d83419ac94095764f87465ec2606bac59
mohd9121 <nazeernajju4684@gmail.com>
2025-09-14 23:24:29 +0530

    Initial Commit:Project with Basic Setup

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from 'react-router'
+import { Toaster } from 'react-hot-toast'
+import AppRoutes from './Components/AppRoutes.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+    
+      <Toaster/>
+      <AppRoutes />
+    
+    </BrowserRouter>
+    
+  </StrictMode>
+)
